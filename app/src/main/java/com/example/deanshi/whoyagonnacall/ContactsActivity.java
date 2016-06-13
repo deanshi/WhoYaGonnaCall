36ff317e470785d7e9ad83de72be99de974f5694
Dean Shi <deanshi@Deans-MacBook-Pro-2.local>
2016-06-13 15:55:41 -0400

    Added contact filtering

--- app/src/main/java/com/example/deanshi/whoyagonnacall/ContactsActivity.java
+++ app/src/main/java/com/example/deanshi/whoyagonnacall/ContactsActivity.java
@@ -5,41 +5,40 @@ import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.widget.SimpleCursorAdapter;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.SearchView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ContactsActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+public class ContactsActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String[] GETTING_FROM = new String[] { ContactsContract.Contacts.DISPLAY_NAME };
     private static final int[] POSTING_TO = new int[] { android.R.id.text1 };
-    private static final String[] PROJECTION = new String[] {ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME};
-    private static final String SELECTION = "((" +
-                    ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND (" +
-                    ContactsContract.Contacts.DISPLAY_NAME + " != '') AND (" +
-                    ContactsContract.Contacts.HAS_PHONE_NUMBER + "))";
+    private static final String[] PROJECTION = new String[] { ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME };
+    private static String SELECTION = "((" +
+            ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND (" +
+            ContactsContract.Contacts.DISPLAY_NAME + " != '') AND (" +
+            ContactsContract.Contacts.HAS_PHONE_NUMBER + "))";
+    private static String contactName = "";
     SimpleCursorAdapter contactsCursorAdapter;
-
+    Uri contactUri;
 
     @BindView(R.id.search_contacts_view)
     SearchView contactsSearchView;
 
     @BindView(android.R.id.list)
     ListView contactsListView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +50,6 @@ public class ContactsActivity extends ListActivity implements LoaderManager.Load
         createContactsAdapter();
         getLoaderManager().initLoader(0, null, this);
         setupSearchView();
-
     }
 
     public void createProgressBar() {
@@ -61,7 +59,6 @@ public class ContactsActivity extends ListActivity implements LoaderManager.Load
         progressBar.setIndeterminate(true);
         getListView().setEmptyView(progressBar);
 
-        // Must add the progress bar to the root of the layout
         ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
         root.addView(progressBar);
     }
@@ -75,30 +72,32 @@ public class ContactsActivity extends ListActivity implements LoaderManager.Load
         contactsSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                contactsCursorAdapter.getFilter().filter("B");
-                contactsCursorAdapter.notifyDataSetChanged();
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String contactText) {
-                Log.i("QueryTextChange", "Text has been changed");
+                Log.i("QueryTextChange", "Text has been changed: " + contactText);
                 if (TextUtils.isEmpty(contactText)) {
-                    contactsListView.clearTextFilter();
+                    contactName = "";
                 } else {
-                    contactsCursorAdapter.getFilter().filter("B");
-                    contactsCursorAdapter.notifyDataSetChanged();
+                    contactName = contactText;
                 }
-                contactsCursorAdapter.notifyDataSetChanged();
+                getLoaderManager().restartLoader(0, null, ContactsActivity.this);
                 return true;
             }
         });
-        contactsSearchView.setSubmitButtonEnabled(true);
+        contactsSearchView.setSubmitButtonEnabled(true); //Not used in my case.
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this, ContactsContract.Contacts.CONTENT_URI, PROJECTION, SELECTION, null, null);
+        if (TextUtils.isEmpty(contactName)) {
+            contactUri = ContactsContract.Contacts.CONTENT_URI;
+        } else {
+            contactUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, Uri.encode(contactName));
+        }
+        return new CursorLoader(this, contactUri, PROJECTION, SELECTION, null, null);
     }
 
     @Override
